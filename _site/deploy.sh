b663fc0145bd2e45421432c011bc5ed8d8ebe145
Li Xinyang <lixinyang1026@gmail.com>
2016-04-20 19:53:26 +0800

    Embed FrontSeat Discourse

--- _site/deploy.sh
+++ _site/deploy.sh
@@ -3,5 +3,15 @@ set -o errexit
 rm -rf public
 mkdir public
 
-git config --global user.email "lixinyang1026@gmail.com"
-git config --global user.name "Li Xinyang"
+git config --global user.email "rebot@frontseat.com"
+git config --global user.name "Rebot"
+
+jekyll build
+
+cp -r ./_site/* ./public
+
+cd ./public
+git init
+git add .
+git commit -m "Deploy to GitHub Page"
+git push -f "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
