3a31ea5d497ca3e606a6365a47faa8696ebc9a54
Li Xinyang <lixinyang1026@gmail.com>
2016-04-21 12:29:43 +0800

    Add image resize script

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var imageResize = require('gulp-image-resize');
+
+gulp.task('default', function() {
+  gulp.src('./_images/*.{jpg,png}')
+  .pipe(imageResize({
+    width: 740,
+    upscale: false
+  }))
+  .pipe(gulp.dest('./images'))
+});
